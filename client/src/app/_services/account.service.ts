d7940763180b547e79cda3fb566e82e79fadf864
Aditya Alugani <marvelsuniverse11967@gmail.com>
2023-04-06 15:05:10 +0530

    End of section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private currentUserSource=new BehaviorSubject<User | null>(null);
+  userSource$=this.currentUserSource.asObservable();
+
+
+  baseUrl="https://localhost:5001/api/"
+  constructor(private http:HttpClient) { }
+  setcurrentsource(user_:User|null)
+  {
+    this.currentUserSource.next(user_)
+  }
+  login(model:any)
+  {
+    return this.http.post<User>(this.baseUrl+"account/login",model).pipe(
+      map((response:User)=>{
+        if(response)
+        {
+          localStorage.setItem('user',JSON.stringify(response));
+          this.currentUserSource.next(response);
+        }
+      })
+    )
+  }
+  register(model:any)
+  {
+    return this.http.post<User>(this.baseUrl+"account/register",model).pipe(
+      map(
+        user=>{
+          console.log(user);
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      )
+    )
+  }
+  logout():void
+  {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model:any={};
+  @Output() eventemmiter=new EventEmitter();
+  constructor(private accountservice:AccountService) { }
+
+  ngOnInit(): void {
+  }
+  register()
+  {
+    this.accountservice.register(this.model).subscribe({
+      next:()=>{},
+      error:error=>console.log(error)
+    });
+  }
+  cancel()
+  {
+    this.eventemmiter.emit(false);
+  }
+
+}
